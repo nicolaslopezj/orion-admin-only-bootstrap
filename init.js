c87b0fa5d613dee935ac5e9610cc7eef1f19fa95
Nicolás López <nicolaslopezj@me.com>
2015-07-22 01:08:53 -0300

    initial commit

---
+++ init.js
@@ -0,0 +1,2 @@
+ReactiveTemplates.set('layout', 'onlyBootstrap_layout');
+ReactiveTemplates.set('outAdminLayout', 'onlyBootstrap_outAdmin');
---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'nicolaslopezj:orion-admin-only-bootstrap',
+  summary: 'Load boostrap and fontawesome only in the admin',
+  version: '1.0.0',
+  git: 'https://github.com/nicolaslopezj/orion-admin-only-bootstrap'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.use(['meteor-platform', 'orionjs:bootstrap@1.2.1']);
+  api.addFiles(['init.js', 'templates.html'], 'client');
+});
