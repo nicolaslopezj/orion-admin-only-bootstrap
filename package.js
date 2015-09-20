6e9be4c4f0f320d4abcdf2ff584c0097ce0ab8ba
Nicolás López <nicolaslopezj@me.com>
2015-09-20 11:16:32 -0300

    fix #1 & fix #2

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'nicolaslopezj:orion-admin-only-bootstrap',
   summary: 'Load boostrap and fontawesome only in the admin',
-  version: '1.0.0',
+  version: '1.1.0',
   git: 'https://github.com/nicolaslopezj/orion-admin-only-bootstrap'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use(['meteor-platform', 'orionjs:bootstrap@1.2.1']);
-  api.addFiles(['init.js', 'templates.html'], 'client');
+  api.addFiles(['init.js', 'templates.html', 'templates.js'], 'client');
 });
---
+++ templates.js
@@ -0,0 +1,3 @@
+Template.onlyBootstrap_layout.onRendered(function() {
+  $.getScript('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js');
+});
